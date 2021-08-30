b9d2df6bb86f9178057ee25dbe7fd4bb7aaa2f7e
Vikram Singh Shahi <vikramsinghshahi@gmail.com>
2021-08-30 12:15:59 +0530

    update

--- block-BNaabd/data.js
+++ block-BNaabd/data.js
@@ -1,9 +1,26 @@
 var http = require('http');
+var qs = require('querystring');
 
 var server = http.createServer(handleServer);
 
 function handleServer(req, res) {
-  req.on();
+  var store = '';
+  req.on('data', (chunk) => {
+    store = store + chunk;
+  });
+  req.on('end', () => {
+    if (req.method === 'POST' && req.url === '/json') {
+      res.setHeader('Content-Type', 'application/json');
+      res.end(store);
+    }
+
+    if (req.method === 'POST' && req.url === '/form') {
+      var formData = qs.parse(store);
+
+      res.end(JSON.stringify(formData));
+      console.log(formData);
+    }
+  });
 }
 
 server.listen(7000, () => console.log('server is listening on 3000'));
